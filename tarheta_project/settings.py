744ca78993ee2831343b11922fbce6729fbdf2ba
UnknownArtist <bonggolto.karlg9@gmail.com>
2026-09-04 02:45:47 +0800

    ad

--- tarheta_project/settings.py
+++ tarheta_project/settings.py
@@ -83,13 +83,22 @@ WSGI_APPLICATION = 'tarheta_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            database_url,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
